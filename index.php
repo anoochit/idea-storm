eb544208df659283d415107da6db43a20ed578c4
anoochit <anoochit@e6185ede-da55-11dd-9d6a-15002d8776aa>
2009-01-24 17:50:44 +0000

    "update load module & theme in index file"

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+/*
+Copyright (C) 2008 Anoochit Chalothorn <anoochit@redlinesoft.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+session_start();
+
+/**
+ * load APIs
+ */
+include_once("config.inc.php");
+include_once('include/adodb/adodb.inc.php');
+include_once('include/adodb/adodb-pager.inc.php');
+require_once('include/adodb/adodb-active-record.inc.php');
+include_once('include/adodb/adodb-exceptions.inc.php');
+
+$db = NewADOConnection("mysql://".$cfg_username.":".$cfg_password."@".$cfg_host."/".$cfg_db);
+ADOdb_Active_Record::SetDatabaseAdapter($db);
+//$db->debug=1;
+
+/**
+ * load smarty
+ */
+
+define ("SMARTY_DIR", "include/smarty/");
+require_once (SMARTY_DIR."Smarty.class.php");
+$smarty = new Smarty;
+$smarty->compile_dir = "cache";
+$smarty->template_dir = "theme/".$cfg_theme."/";
+$smarty->assign("site_theme", $cfg_theme);
+
+
+/**
+ *  Align URI if deploy in sub directory
+ */
+if (!empty($cfg_subdir)) {
+	$_SERVER['REQUEST_URI']=substr($_SERVER['REQUEST_URI'],strlen($cfg_subdir)+1);
+}
+
+/**
+ *  Create strap value
+ */
+$strap=split("/",$_SERVER['REQUEST_URI']);
+$mod=$strap[1]; // mod
+$act=$strap[2]; // command or action
+$file=$strap[3]; // files
+$item=$strap[4]; // item id
+
+
+/**
+ * load language file
+ */
+if (file_exists("language/lang-".$cfg_lang.".php")) {
+	include_once("language/lang-".$cfg_lang.".php");
+} else {
+	include_once("language/lang-en.php");
+}
+
+
+/**
+ *  load bootstrape
+ */
+include_once("include/bootstrap.inc.php");
+
+
+/**
+ *  load module
+ */
+if (($mod!="") AND ($act=="") AND ($file=="") AND ($item=="")) {
+	// find template 
+	if (file_exists("theme/".$cfg_theme."/page-".$mod.".tpl")) {
+		// assign template file
+		$template="page-".$mod;
+	} else {
+		// cannot fine template file use page default
+		$template="page";
+	}
+	// find pagefile
+	if (file_exists("module/".$mod.".php")) {
+		// assign page filename
+		$pagefile=$mod;		
+	} else {
+		// assign page filename to page not found
+		$pagefile="page-not-found";
+	}
+} else if (($mod=="") AND ($act=="")  AND ($file=="") AND ($item=="")) {
+	// index page
+	$template="index";
+	$pagefile="home";
+}
+
+
+/**
+ * set value to template engine
+ */
+$smarty->assign("site_url", $cfg_url);
+
+
+/**
+ *  set footer value
+ * */
+ob_start();			
+require_once("include/footer.inc.php");
+$content = ob_get_contents();
+ob_end_clean();
+$smarty->assign("site_footer", $content);
+
+/**
+ * set body value
+ */
+ob_start();	
+include("module/".$pagefile.".php");
+$content = ob_get_contents();
+ob_end_clean();
+$smarty->assign("site_body", $content);
+
+
+$smarty->display($template.".tpl");	
+
+?>
