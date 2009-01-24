e1606731c548134459b738b2a7616578a565a737
anoochit <anoochit@e6185ede-da55-11dd-9d6a-15002d8776aa>
2009-01-24 15:39:07 +0000

    "add config.inc.php"

---
+++ config.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/**
+ * Database Setting
+ */
+$cfg_db="eport";
+$cfg_host="localhost";
+$cfg_username="raziel";
+$cfg_password="monalisa";
+
+/**
+ *  Misc Initial Config
+ */
+$cfg_url="http://brianstorm.redlinesoft.net";
+$cfg_subdir="";
+$cfg_dir="D:\\redlinesoft\www\brainstorm\trunk";
+$cfg_lang="english";
+$cfg_theme="default";
+$cfg_offline="n";
+
+
+/**
+ * SMTP
+ */
+$cfg_smtp_host="localhost";
+$cfg_smtp_port="25";
+
+/**
+ * Email administration
+ */
+
+$cfg_admin_name="support";
+$cfg_admin_email="support@brainstorm.redlinesoft.net";
+
+
+
+?>
