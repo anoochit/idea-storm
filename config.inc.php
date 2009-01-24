c50b304aa3f18ad48128f85c00ad672555e933f3
anoochit <anoochit@e6185ede-da55-11dd-9d6a-15002d8776aa>
2009-01-24 17:46:37 +0000

    "update config.inc.php"

--- config.inc.php
+++ config.inc.php
@@ -1,4 +1,20 @@
 <?php
+/*
+Copyright (C) 2008 Anoochit Chalothorn <anoochit@redlinesoft.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 /**
@@ -12,9 +28,9 @@ $cfg_password="monalisa";
 /**
  *  Misc Initial Config
  */
-$cfg_url="http://brianstorm.redlinesoft.net";
+$cfg_url="http://brainstorm.redlinesoft.net";
 $cfg_subdir="";
-$cfg_dir="D:\\redlinesoft\www\brainstorm\trunk";
+$cfg_dir="D:\\redlinesoft\\www\\brainstorm\\trunk";
 $cfg_lang="english";
 $cfg_theme="default";
 $cfg_offline="n";
