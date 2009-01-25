6bda17c6be8cc3ec82f79e4e1b127a0db36d3b1c
anoochit <anoochit@e6185ede-da55-11dd-9d6a-15002d8776aa>
2009-01-25 02:25:54 +0000

    "add system class"

---
+++ include/ideastorm/class.system.php
@@ -0,0 +1,45 @@
+<?
+
+class iSystem {
+	var $url;	
+	
+	function __construct($url,$theme) {
+		$this->url=$url;
+	}
+	
+	function goPageDelay($uri,$time=1){
+		?>
+		<meta http-equiv="refresh" content="<?=$time; ?>;URL=<?=$this->url."/".$uri; ?>" />
+		<?
+	}
+	
+	function showMessage($type=1,$message){
+		?>
+		<div id="messagebox">
+			<div name="icon">
+			<?
+				switch ($type) {
+					case "1" : ?><img src="<?=$this->url;?>/image/messagebox_critical.png" border="0" ><?
+					break;
+					case "2" : ?><img src="<?=$this->url;?>/image/messagebox_warning.png" border="0" ><?
+					break;
+					default:
+						?><img src="<?=$this->url;?>/image/messagebox_info.png" border="0" ><?
+					break;
+				}
+			
+			?>
+			</div>
+			<div name="message"><?=$message;?></div>
+		</div>
+		<?
+	}
+	
+	function setSession($fullname,$memid){
+		$_SESSION['memid']=$memid;
+		$_SESSION['fullname']=$fullname;
+	}
+	
+}
+
+?>
