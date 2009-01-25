7ebfc6291d98c4f2dfb011b468f067f1e600e556
anoochit <anoochit@e6185ede-da55-11dd-9d6a-15002d8776aa>
2009-01-25 02:28:52 +0000

    "update member ORM class"

--- include/ideastorm/class.member.php
+++ include/ideastorm/class.member.php
@@ -6,4 +6,25 @@ class iMember extends ADOdb_Active_Record{
 
 }
 
+class iMemberUtil {
+
+	function memberExist($openid_identity){
+		$obmem=new iMember();
+		$obmem->Load("openid = ? ",array($openid_identity));
+		if (($obmem->memid)>0) {
+			$res=true;
+		} else {
+			$res=false;
+		}
+		return $res;
+	}
+	
+	function loadMember($openid_identity) {
+		$obmem=new iMember();
+		$obmem->Load("openid = ? ",array($openid_identity));
+		return $obmem;
+	}
+	
+}
+
 ?>
